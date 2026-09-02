37aa81384a5d22121d58cd5e71fc45bd9d2c458b
Mike <michael.robeson@colorado.edu>
2026-09-02 15:36:20 -0500

    cleaned up organism_name check code

--- rescript/parse_silva_taxonomy.py
+++ rescript/parse_silva_taxonomy.py
@@ -8,6 +8,7 @@
 
 import re
 import pandas as pd
+import numpy as np
 from skbio.tree import TreeNode
 from collections import OrderedDict
 
@@ -99,10 +100,13 @@ def _prep_taxmap(taxmap):
     # Check if there are any NaNs in organism_name.
     # If so, pull last taxonomy item from the taxonomy path and use
     # that for the organism_name.
-    if taxmap.loc[:, 'organism_name'].isna().any():
-        taxmap.loc[:, 'organism_name'] = taxmap.loc[
-            :, 'organism_name'].fillna(taxmap.loc[:, 'path'].apply(
-                get_last_taxmap_taxpath_label))
+    taxmap['organism_name'] = np.where(
+        taxmap['organism_name'].isna(), taxmap['path'].apply(
+                get_last_taxmap_taxpath_label), taxmap['organism_name'])
+    # if taxmap.loc[:, 'organism_name'].isna().any():
+    #    taxmap.loc[:, 'organism_name'] = taxmap.loc[
+    #        :, 'organism_name'].fillna(taxmap.loc[:, 'path'].apply(
+    #            get_last_taxmap_taxpath_label))
     # Return silva taxonomy dataframe map indexed by accession.seqtart.seqstop
     # This is how the coreesponding FASTA file IDs are structured
     taxmap.index = taxmap.index + '.' + taxmap.start + '.' + taxmap.stop
